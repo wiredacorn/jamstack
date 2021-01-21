4c0f18e6a076b895acceecba4e4de1b44d285b70
wiredacorn <dev@wiredacorn.com>
2021-01-21 00:41:07 -0700

    add function

--- src/_functions/weatherapi.js
+++ src/_functions/weatherapi.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 exports.handler = async function(event, context, callback) {
   const { lat, lon } = event.queryStringParameters;
-  const url =`https://api.openweathermat.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_API_KEY}&units=imperial`;
+  const url =`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_API_KEY}&units=imperial`;
   const response = await axios.get(url);
   callback(null,{
     statusCode: 200,
